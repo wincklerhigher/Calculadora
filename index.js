f6e4e219072ff5b8d939926b8324b08099140d85
WillianWinckler <73580555+wincklerhigher@users.noreply.github.com>
2024-07-01 20:21:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+
+const appName = 'Calculadora';
 
 AppRegistry.registerComponent(appName, () => App);
